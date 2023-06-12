723ed91135a55734e4aa246b0f5163a51ae85c28
TerukiToyonaga <127729072+teru5678@users.noreply.github.com>
2023-06-12 04:57:48 +0000

    ログインヘッダー

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
@@ -7,4 +8,14 @@ class UsersController < ApplicationController
 
   def edit
   end
+  
+  private
+    def user_admin
+       @users = User.all
+       if  current_user.admin == false
+           redirect_to root_path
+       else
+           render action: "index"
+       end
+    end
 end
