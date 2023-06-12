e335d21c4de310399253acd305f5bcdf9bd59dce
TerukiToyonaga <127729072+teru5678@users.noreply.github.com>
2023-06-12 05:35:13 +0000

    [Add]ゲストログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
   resources :users, only:[:index,:show,:edit]
   resources :shops, only:[:new,:index,:show,:edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
